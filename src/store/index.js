26884bc56c278d362bb81e69bc192041301da79e
webjimmylau <web_jimmy_lau@126.com>
2018-08-08 18:00:37 +0800

    利用localstorage.city将城市缓存在浏览器，注意对象的缓存

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+let defaultCity = {
+  id: 1,
+  spell: 'beijing',
+  name: '北京'
+}
+if(localStorage.city){
+  defaultCity = JSON.parse(localStorage.city) // 字符串转对象
+}
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    city: {
-      id: 1,
-      spell: 'beijing',
-      name: '北京'
-    }
+    city: defaultCity
   },
   mutations: {
     changeCity(state, payload){
       state.city = payload
+      localStorage.city = JSON.stringify(payload) // 当为对象是，要转字符串
     }
   },
   actions: {
